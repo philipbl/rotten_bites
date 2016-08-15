9c15ced388da0ff7ad8cd8bdb9f08682f5456d48
Philip Lundrigan <philiplundrigan@gmail.com>
2016-08-15 15:54:00 -0600

    Add duration to email

--- rot_check.py
+++ rot_check.py
@@ -8,6 +8,30 @@ import click
 import requests
 import rotten_bites
 
+def duration_human(raw_seconds):
+    seconds = round(raw_seconds)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    years, days = divmod(days, 365.242199)
+
+    duration = []
+    if years > 0:
+        duration.append('{} year{}'.format(years, 's'*(years != 1)))
+    else:
+        if days > 0:
+            duration.append('{} day{}'.format(days, 's'*(days != 1)))
+        if hours > 0:
+            duration.append('{} hour{}'.format(hours, 's'*(hours != 1)))
+        if minutes > 0:
+            duration.append('{} minute{}'.format(minutes, 's'*(minutes != 1)))
+        if seconds > 0:
+            duration.append('{} second{}'.format(seconds, 's'*(seconds != 1)))
+        if seconds == 0:
+            duration.append('{} seconds'.format(raw_seconds))
+
+    return ' '.join(duration)
+
 
 @click.command()
 @click.argument('directory')
@@ -51,16 +75,18 @@ def main(directory, to, from_, domain, api_key):
         nonlocal missing_files
         missing_files += 1
 
+    now = time.time()
     rotten_bites.run(directory, added_cb=added_cb, updated_cb=updated_cb,
                      nothing_cb=nothing_cb, file_error_cb=file_error_cb,
                      hash_error_cb=hash_error_cb, missing_cb=missing_cb)
+    run_time = time.time() - now
 
     # Send email
     text = io.StringIO()
-    text.write('{} files scanned, {} new, {} updated, {} missing, {} errors.'.format(
+    text.write('Ran for {}.\n'.format(duration_human(run_time)))
+    text.write('{} files scanned, {} new, {} updated, {} missing, {} errors.\n'.format(
                len(added_files) + len(update_files) + len(nothing_files) + len(hash_error_files),
                len(added_files), len(update_files), len(missing_files), len(hash_error_files)))
-
     text.write('\n\n')
 
     def print_files(files):
@@ -73,22 +99,27 @@ def main(directory, to, from_, domain, api_key):
     if hash_error_files:
         text.write('Files with bit rot:\n')
         text.write(print_files(hash_error_files))
+        text.write('\n\n')
 
     if file_error_files:
         text.write('Files unable to open:\n')
         text.write(print_files(file_error_files))
+        text.write('\n\n')
 
     if added_files:
         text.write('Added files:\n')
         text.write(print_files(added_files))
+        text.write('\n\n')
 
     if update_files:
         text.write('Updated files:\n')
         text.write(print_files(update_files))
+        text.write('\n\n')
 
     if missing_files:
         text.write('Missing files:\n')
         text.write(print_files(missing_files))
+        text.write('\n\n')
 
     request = requests.post(
         'https://api.mailgun.net/v3/{0}/messages'.format(domain),
@@ -102,7 +133,6 @@ def main(directory, to, from_, domain, api_key):
         print("Error: Incorrect API key")
 
     result = request.json()
-
     if 'Domain not found' in result['message']:
         print("Error: {}".format(result['message']))
 
